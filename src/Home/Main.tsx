1a05fea6319d9553586531bafa3a72e31c24a03e
Henrique Penha <henrique-dsp34@hotmail.com>
2026-08-11 12:32:48 -0300

    feat: escolhas orit

--- src/Home/Main.tsx
+++ src/Home/Main.tsx
@@ -40,7 +40,7 @@ export default function Main() {
       <Hero videoSrc={bannerVideo} handleModal={handleModal} />
 
       <main className="grid grid-cols-3">
-        <Tile label="DIA DOS PAIS" imageSrc="/bg-pais.jpg" href="/produtos/dia-dos-pais" />
+        <Tile label="ESCOLHAS ORIT" imageSrc="/escolhas-orit-home.jpg" href="/produtos/escolhas-orit" />
         <Tile label="RELÓGIOS" imageSrc={imageWatch} href="/produtos/relogios" />
         <Tile label="NOVIDADES" imageSrc={imageNews} href="/produtos/novidades" />
 
